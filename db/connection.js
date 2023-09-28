6ba181f4308db623a9a0eae89f427c42e372e076
Harshitapurwar <harshitapurwar07@gmail.com>
2023-09-29 00:45:21 +0530

    first entry done!

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const uri = "mongodb+srv://harshitapurwar07:harshita6307@mycluster.q2hmesz.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = "mongodb+srv://harshitapurwar07:harshita6307@mycluster.q2hmesz.mongodb.net/test?retryWrites=true&w=majority";
+const User = require('./models/UserSchema.js')
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -16,5 +16,17 @@ db.on('error', (err) => {
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
+
+
+const user = new User({
+    id:1,
+    firstName:'Sparsh',
+    lastName :'Jhariya',
+    email:'sparsh3435@gmail.com'
+  })
+
+  user.save().then(result => {
+    console.log('Document saved successfully!');
+  });
 
 module.export = db;
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const db = require('./db/connection')
+const User = require('./db/models/UserSchema.js')
 
 const app = express();
 
-const port = 5000
+const port = 5001;
 
 // console.log(db.adminCommand({ listDatabases: 1, filter: { size: { $gt: 100000000 } } }))
 
-app.listen(port , () =>{console.log('server listening on port 5000')})
+// const user = new User({
+//   id:1,
+//   firstName:'Sparsh',
+//   lastName :'Jhariya',
+//   email:'sparsh3435@gmail.com'
+// })
+
+
+app.listen(port , () =>{console.log('server listening on port 5001')})
