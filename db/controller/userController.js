79a469d1e3dfc9e0658791c11d7015039e0d0560
Harshitapurwar <harshitapurwar07@gmail.com>
2023-09-29 11:41:16 +0530

    new file

--- db/controller/userController.js
+++ db/controller/userController.js
@@ -1,10 +1,11 @@
 const User = require('../models/UserSchema')
-async function addUser(user_id , f_name , l_name , user_email){
+async function addUser(user_id , f_name , l_name , user_email,user_skill){
     const user = new User({
         id:user_id,
         firstName:f_name,
         lastName :l_name,
-        email:user_email
+        email:user_email,
+        skills:user_skill
       })
     
       user.save().then(result => {
