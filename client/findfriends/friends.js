2e54359462348f09bf92146bc6bd52ee4b06e008
Tanya Purwar <tanyapurwar122@gmail.com>
2023-10-01 16:36:09 +0530

    updates

--- client/findfriends/friends.js
+++ client/findfriends/friends.js
@@ -1,1 +1,38 @@
-let divuserrow=document.createElement('div');
+data = [{
+    name:"sparsh",
+    email:"sdadf"
+},
+{
+    name:"adfdsf",
+    email:"adfsg"
+},
+{
+    name:"adfdsf",
+    email:"adfsg"
+}
+]
+addUsers(data);
+function addUsers(data){
+    const user=document.getElementById('friends-body');
+    for(let i=0;i<Object.keys(data).length;i++){
+        const userrow=document.createElement('div');
+        user.appendChild(userrow);
+        userrow.classList.add("user-row");
+        const userinfo=document.createElement('div');
+        userrow.appendChild(userinfo);
+        userinfo.classList.add("user-info");
+        const img=document.createElement('img');
+        userinfo.appendChild(img);
+        img.src='';
+        img.alt = 'User';
+        const span=document.createElement('span');
+        userinfo.appendChild(span);
+        span.innerText = `${data[i].name}`;
+        const button=document.createElement('button');
+        button.classList.add("action-follow");
+        userrow.appendChild(button);
+        button.innerText="Add in team";
+
+    }
+}
+
