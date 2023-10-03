d0846119d1f053780cae4c362884740592ba409a
Tanya Purwar <tanyapurwar122@gmail.com>
2023-10-03 15:20:19 +0530

    checked

--- client/findfriends/friends.js
+++ client/findfriends/friends.js
@@ -1,6 +1,5 @@
 // data = [{
 //     name:"sparsh",
-<<<<<<< HEAD
 //     email:"Web , App"
 // },
 // {
@@ -14,9 +13,6 @@
 // {
 //     name:"adfdsf",
 //     email:"adfsg"
-=======
-//     email:"sdadf"
->>>>>>> b48129c6d993d6090dddfe9ef02e97fdc8a0b19c
 // },
 // {
 //     name:"adfdsf",
@@ -27,7 +23,6 @@
 //     email:"adfsg"
 // }
 // ]
-<<<<<<< HEAD
 fetch('http://localhost:5500/user/all')
     .then((response) => response.json())
     .then((data) => {
@@ -42,24 +37,6 @@ fetch('http://localhost:5500/user/all')
     });
 
 addUsers(data);
-=======
-// addUsers(data);
-
-
-// fetch('http://localhost:5000/user/all')
-//     .then((response) => response.json())
-//     .then((data) => {
-//         // Handle the JSON data here
-//         // console.log('hello')
-//         // console.log(data);
-//         addUsers(data)
-//         // You can now display or manipulate the data in your HTML
-//     })
-//     .catch((error) => {
-//         console.error('Error:', error);
-//     });
-
->>>>>>> b48129c6d993d6090dddfe9ef02e97fdc8a0b19c
 function addUsers(data){
     // console.log(data)
     const user=document.getElementById('friends-body');
@@ -77,14 +54,10 @@ function addUsers(data){
         img.alt = 'User';
         const span=document.createElement('span');
         userinfo.appendChild(span);
-<<<<<<< HEAD
         span.innerText = `${data[i].name} : Skills(${data[i].email})`;
         // const span1=document.createElement('span');
         // userinfo.appendChild(span1);
         // span.innerText = `${data[i].email}`;
-=======
-        span.innerText = `${data[i].firstName}`;
->>>>>>> b48129c6d993d6090dddfe9ef02e97fdc8a0b19c
         const button=document.createElement('button');
         button.classList.add("action-follow");
         userrow.appendChild(button);
